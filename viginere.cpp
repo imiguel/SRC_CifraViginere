ac6d86df3bab36540a95b6332f9352bd3188531f
imiguel <axtcas900>
2015-10-08 18:59:55 +0100

    Criar arrays e copia de strings

--- viginere.cpp
+++ viginere.cpp
@@ -9,6 +9,7 @@ void cifra(){
 	int c = 0;
 	int countToKeyArray = 0;
 	char mensagem[999];
+	
 
 
 	fileMensagem = fopen("mensagem.txt", "r");
@@ -19,19 +20,15 @@ void cifra(){
     */
 	while ((c = fgetc(fileMensagem)) != EOF) {
 		printf("char: %c \n", c); //mostra no ecra os char da mensagem (APAGAR ESTA LINHA)
+		mensagem[countToKeyArray] = c; //copia para o array mensagem a mensagem vinda do ficheiro txt
         countToKeyArray++; //saber o tamanho maximo da mensagem. Vai contando o num de char
     }
-    printf ("\nNum char da mensagem: %d\n\n", countToKeyArray); //LINHA DE TESTE - APAGAR NO FINAL
     char chave[countToKeyArray]; //cria o array de char com o tamanho da mensagem para poder cifrar
-    
+    printf ("\nNum char da mensagem: %d\n\n", countToKeyArray); //LINHA DE TESTE - APAGAR NO FINAL
+    printf("\n\nMENSAGEM: %s\n", mensagem); //LINHA DE TESTE - APAGAR NO FINAL
 
 
 
-	/*          FAZER
-	1. ler o ficheiro letra a letra e guardar a letra num vector char
-	2. ler o ficheiro mensagem.txt e guardar as letras num vector de char
-	3. fazer a correspondencia letra chave vs letra da mensagem
-	*/
 
 
 	fclose(fileMensagem);
@@ -52,6 +49,7 @@ int main(int argc, char* argv[]){
 
 	if( (strcmp(argv[1],"d") == 0) || (strcmp(argv[1],"D") == 0) ){
 		printf("\n\nDentro da DECIFRA\n"); //APAGAR ESTA LINHA NO ANTES DE ENTREGAR
+		decifra();
 	}
 
 	return 0;
