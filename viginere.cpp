ed3c6300fee7d0a6f232b494f996c5beb3cf7d78
imiguel <axtcas900>
2015-10-08 21:42:34 +0100

    Updates

--- viginere.cpp
+++ viginere.cpp
@@ -6,39 +6,61 @@
 void cifra(){
 	FILE *fileChave;
 	FILE *fileMensagem;
-	int c = 0;
+	int m = 0, c = 0, aux = 0;
 	int countToKeyArray = 0;
-	char mensagem[999];
+	char mensagemArray[999];
 	
 
 
+	////////////////
+	//            //
+	//  MENSAGEM  //
+	//            //
+	////////////////
 	fileMensagem = fopen("mensagem.txt", "r");
 
-
-
-<<<<<<< HEAD
-    /*Conta os chars do ficheiro "mensagem.txt". Permite determinar quantos char são necessarios para a chave.
-    */
-	while ((c = fgetc(fileMensagem)) != EOF) {
-		printf("char: %c \n", c); //mostra no ecra os char da mensagem (APAGAR ESTA LINHA)
-		mensagem[countToKeyArray] = c; //copia para o array mensagem a mensagem vinda do ficheiro txt
-        countToKeyArray++; //saber o tamanho maximo da mensagem. Vai contando o num de char
-=======
-    //copia o conteudo do ficheiro "mensagem.txt" para um array de char
-    while ((c = fgetc(fileMensagem)) != EOF) {
-	printf("char: %c \n", c); //mostra no ecra a mensagem
-        count++; //saber o tamanho maximo da mensagem
->>>>>>> origin/master
+    //Conta os chars do ficheiro "mensagem.txt". Permite determinar quantos char são necessarios para a chave.
+	while ((m = fgetc(fileMensagem)) != EOF) {
+		printf("char: %c \n", m);           //mostra no ecra os char da mensagem (APAGAR ESTA LINHA)
+		mensagemArray[countToKeyArray] = m; //copia para o array mensagem a mensagem vinda do ficheiro txt
+        countToKeyArray++;                  //saber o tamanho maximo da mensagem. Vai contando o num de char
     }
-    char chave[countToKeyArray]; //cria o array de char com o tamanho da mensagem para poder cifrar
-    printf ("\nNum char da mensagem: %d\n\n", countToKeyArray); //LINHA DE TESTE - APAGAR NO FINAL
-    printf("\n\nMENSAGEM: %s\n", mensagem); //LINHA DE TESTE - APAGAR NO FINAL
-
-
-
-
+    //printf ("\nNum char da mensagem: %d\n\n", countToKeyArray); //LINHA DE TESTE - APAGAR NO FINAL
+    //printf("\n\nMENSAGEM: %s\n", mensagem); //LINHA DE TESTE - APAGAR NO FINAL
 
 	fclose(fileMensagem);
+
+
+	/////////////
+	//         //
+	//  CHAVE  //
+	//         //
+	/////////////
+	fileChave = fopen("chave.txt", "r");
+	char chaveArray[countToKeyArray]; //cria o array de char com o tamanho da mensagem para poder cifrar
+
+	printf("\n"); //APAGAR
+	//copiar do ficheiro cahev.txt para o array chave
+	while( (c = fgetc(fileChave)) != EOF ){
+		printf("char: %c \n", c);      //mostra no ecra os char da mensagem (APAGAR ESTA LINHA)
+		chaveArray[aux] = c;
+		aux++;
+	}
+	fclose(fileChave);
+
+
+
+
+
+
+
+
+	//TESTES
+	printf ("\nNum char da mensagem: %d", countToKeyArray); //LINHA DE TESTE - APAGAR NO FINAL
+	printf ("\n   Num char da chave: %d\n\n", aux); //LINHA DE TESTE - APAGAR NO FINAL
+    printf("\n\nMENSAGEM: %s", mensagemArray); //LINHA DE TESTE - APAGAR NO FINAL
+    printf("\n   CHAVE: %s\n", chaveArray); //LINHA DE TESTE - APAGAR NO FINAL
+
 }
 
 void decifra(){
@@ -50,7 +72,7 @@ int main(int argc, char* argv[]){
 	char input[2];
 
 	if( (strcmp(argv[1],"c") == 0) || (strcmp(argv[1],"C") == 0) ){
-		printf("\n\nDentro da CIFRA\n"); //APAGAR ESTA LINHA NO ANTES DE ENTREGAR
+		printf("\nDentro da CIFRA\n"); //APAGAR ESTA LINHA NO ANTES DE ENTREGAR
 		cifra();
 	}
 
