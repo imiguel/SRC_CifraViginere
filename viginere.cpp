fc344711144d782373cc6b52e29b4fc0f7e726c0
imiguel <axtcas900>
2015-10-08 18:41:30 +0100

    .

--- viginere.cpp
+++ viginere.cpp
@@ -7,8 +7,7 @@ void cifra(){
 	FILE *fileChave;
 	FILE *fileMensagem;
 	int c = 0;
-	int count = 0;
-	char chave[999];
+	int countToKeyArray = 0;
 	char mensagem[999];
 
 
@@ -16,19 +15,20 @@ void cifra(){
 
 
 
-    //copia o conteudo do ficheiro "mensagem.txt" para um array de char
+    /*Conta os chars do ficheiro "mensagem.txt". Permite determinar quantos char são necessarios para a chave.
+    */
 	while ((c = fgetc(fileMensagem)) != EOF) {
-		printf("char: %c \n", c); //mostra no ecra a mensagem
-        count++; //saber o tamanho maximo da mensagem
+		printf("char: %c \n", c); //mostra no ecra os char da mensagem (APAGAR ESTA LINHA)
+        countToKeyArray++; //saber o tamanho maximo da mensagem. Vai contando o num de char
     }
-    printf ("\nNum char da mensagem: %d", count); //LINHA DE TESTE - APAGAR NO FINAL
-
+    printf ("\nNum char da mensagem: %d\n\n", countToKeyArray); //LINHA DE TESTE - APAGAR NO FINAL
+    char chave[countToKeyArray]; //cria o array de char com o tamanho da mensagem para poder cifrar
     
 
 
 
 	/*          FAZER
-	1. ler o ficheiro letra a letra e guardar a letra num vecotr char
+	1. ler o ficheiro letra a letra e guardar a letra num vector char
 	2. ler o ficheiro mensagem.txt e guardar as letras num vector de char
 	3. fazer a correspondencia letra chave vs letra da mensagem
 	*/
@@ -48,7 +48,6 @@ int main(int argc, char* argv[]){
 	if( (strcmp(argv[1],"c") == 0) || (strcmp(argv[1],"C") == 0) ){
 		printf("\n\nDentro da CIFRA\n"); //APAGAR ESTA LINHA NO ANTES DE ENTREGAR
 		cifra();
-		printf("\n");
 	}
 
 	if( (strcmp(argv[1],"d") == 0) || (strcmp(argv[1],"D") == 0) ){
